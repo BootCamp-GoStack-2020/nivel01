f11799b52cfc6d032fdf204ca7780d3ed7b406d6
viannaandreBR <viannaandresouza@gmail.com>
2020-08-19 20:00:47 -0300

    Integrate Frontend -> Backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api'
+
 import Header from './components/Header';
 import './App.css';
-import backgroundImage from './assets/background.jpg';
+//import backgroundImage from './assets/background.jpg';
+//<img width={900}src={backgroundImage}/>
 
 function App(){
  
-  const [projects, setProjects ] = useState([ 'Desenvolvimento de App', 'Front-End Web']);
+  const [projects, setProjects ] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response=> {
+      setProjects(response.data);
+ 
+    })
+  }, [projects]);
 
   function handleAddProject(){
 //    projects.push(`Novo Projeto ${Date.now()}`);
@@ -20,9 +30,8 @@ function App(){
       
       <Header title="Projects"/>
      
-      <img width={900}src={backgroundImage}/>
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
