215e1b59dc13c40036af349c4dda9994585d914b
viannaandreBR <viannaandresouza@gmail.com>
2020-08-19 14:23:45 -0300

    Props: Parent & Child

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,32 @@ import Header from './components/Header';
 
 function App(){
  
+  const projects = [ 'Desenvolvimento de App', 'Front-End Web']
   return (
-
     <>
-      <Header/>
-      <br></br>
+      <Header title="HomePage">
+        <ul>
+          <li>Login</li>
+        </ul>
+      </Header>
       <hr/>
-      <h1>Ola</h1>
+      <h1>Ola Estou no Header #1</h1>
+      <br></br>
+      
+      <Header title="Projects">
+        <ul>
+          <li>Home</li>
+          <li>Tecnologias</li>
+          <li>Produtos</li>
+          <li>Sobre Nós</li>
+        </ul>
+      </Header>
+      <hr/>
+      <h1>Ola Estou no Header #2</h1>
+      <br></br>
+      
     </>
-  )
+  );
   
 }
 
