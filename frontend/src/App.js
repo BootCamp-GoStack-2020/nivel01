9a477531cd1f9d0fd13147898979682c818530e1
viannaandreBR <viannaandresouza@gmail.com>
2020-08-19 14:05:13 -0300

    Component with React Fragment <></>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,13 @@ import Header from './components/Header';
 function App(){
  
   return (
-    <div>
+
+    <>
       <Header/>
       <br></br>
       <hr/>
       <h1>Ola</h1>
-    </div>
+    </>
   )
   
 }
