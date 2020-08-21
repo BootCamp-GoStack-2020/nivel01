6c846ab75e9b4b1861bd1cbce6b717c6f55c2397
viannaandreBR <vianna_andre@hotmail.com@users.noreply.github.com>
2020-08-21 16:13:11 -0300

    Build .js from TypeScript

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 
+import { helloWorld }from './routes';
+
 const app = express();
 
+app.get('/', helloWorld);
 
-app.get('/', (request,response) => {
-  return response.json({message: 'Hello World, Again !!!'})
-})
+// app.get('/', (request,response) => {
+//   return response.json({message: 'Hello World, Again !!!'})
+// })
 app.listen(3333);
