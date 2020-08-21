97ef1bc75aec57786ee0fa78cbe587b404118d99
viannaandreBR <vianna_andre@hotmail.com@users.noreply.github.com>
2020-08-21 02:42:07 -0300

    Connect Mobile to Backend

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
-
-
+import React, {useEffect, useState } from 'react';
 import { View, Text, StyleSheet, StatusBar } from 'react-native'
 
+
+import api from './services/api';
+
 export default function App(){
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response=> {
+     console.log(response.data);
+     setProjects(response.data);
+    });
+  }, []);
+
+
+  
   return (
     
     <>
        <StatusBar barStyle="ligh-content" backgroundColor="#7159C1"/>
         <View style={styles.container}>
           
-          <Text style={styles.title}>Hello GoStack</Text>
+          <Text style={styles.title}>Hello GoStack    Boa Madrugada </Text>
         
         </View>
     </>
