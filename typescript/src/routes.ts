b2899f7895325f77b12d194a75496b3307e284a1
viannaandreBR <vianna_andre@hotmail.com@users.noreply.github.com>
2020-08-21 17:50:30 -0300

    Nivel01 - End

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,23 @@
 import { Request, Response } from 'express';
 
+import createUser from './services/CreateUser';
+
+
+const user = createUser({
+  name: 'Andre',
+  email: 'andre@email.com',
+  password: '123',
+  techs: [
+      'NodeJS', 
+      'ReactJS', 
+      'React Native',
+      { title: 'JavaScript', experience: 100 },
+      
+  ]
+});
+
+
+
 export function helloWorld(request: Request , response: Response)  {
   return response.json({ message: 'Hello World from routes'});
 }
